b467190fafb457065b4a701df58caf3b09806915
Sijawusz Pur Rahnama <sija@sija.pl>
2025-09-02 22:22:00 +0200

    Support `BTC to USD` argument format

--- src/fx-calculator/utils.cr
+++ src/fx-calculator/utils.cr
@@ -5,9 +5,18 @@ module FXCalculator
     # Parses a string representing a money value along with target currency,
     # if present, and returns it as a tuple of `Money` and `Money::Currency`.
     def parse_money_with_currency(str : String) : {Money, Money::Currency}
-      if match = str.match_full(/(?<amount>.*?)\s+(?:in|to)\s+(?<currency>\w+)/)
-        {match["amount"].to_money, Money::Currency.find(match["currency"])}
+      case str
+      when /^(?<base_currency>[a-zA-Z]\w+)\s+(?:in|to)\s+(?<target_currency>\w+)$/
+        # BTC to USD
+        {
+          Money.from_amount(1, Money::Currency.find($~["base_currency"])),
+          Money::Currency.find($~["target_currency"]),
+        }
+      when /^(?<amount>.*?)\s+(?:in|to)\s+(?<target_currency>\w+)$/
+        # $100 to EUR
+        {$~["amount"].to_money, Money::Currency.find($~["target_currency"])}
       else
+        # $100
         {str.to_money, Money.default_currency}
       end
     end
